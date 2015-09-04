60be67355e3a83ffa5ff9ed6ba251b6674ad0867
Darren Reid <darren.j.reid@gmail.com>
2015-09-04 18:20:05 +1000

    New proj + bundle

---
+++ ReactDesktopTest/ReactDesktopTest/$saferootprojectname$.AppMac/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+
+namespace ReactDesktopTest.AppMac.AppMac
+{
+	// Should subclass MonoMac.AppKit.NSResponder
+	[MonoMac.Foundation.Register ("AppDelegate")]
+	public partial class AppDelegate
+	{
+	}
+}
+
---
+++ ReactDesktopTest/ReactDesktopTest/ReactDesktopTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReactDesktopTest.Global" Language="C#" %>
